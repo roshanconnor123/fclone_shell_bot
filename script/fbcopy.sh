73eeed64d143465cfee816e598dcda034fd60312
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-14 06:30:51 +0530

    Update fbcopy.sh

--- script/fbcopy.sh
+++ script/fbcopy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #=============================================================
-# https://github.com/cgkings/fclone_shell_bot
+# https://github.com/roshanconnor123/fclone_shell_bot
 # File Name: fbcopy.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:全盘备份-copy
+# Description:Full drive backup-copy
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
-echo "等待ini配置变量更新"
+echo "Wait for ini configuration variable update"
 sleep 30s
 source ~/fclone_shell_bot/myfc_config.ini
 clear
-echo -e "▣▣▣▣▣▣执行转存▣▣▣▣▣▣"
+echo -e "▣▣▣▣▣▣Perform copy▣▣▣▣▣▣"
 fclone copy "$fclone_name":{$fb_link1} "$fclone_name":{$fb_link2} --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first
-echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕/n"
-echo -e "▣▣▣▣▣▣执行补缺▣▣▣▣▣▣"
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  Completed/n"
+echo -e "▣▣▣▣▣▣Perform Sync▣▣▣▣▣▣"
 fclone sync "$fclone_name":{$fb_link1} "$fclone_name":{$fb_link2} --drive-server-side-across-configs --drive-use-trash=false --stats=1s --stats-one-line -vP --checkers="$fb_chercker" --transfers="$fb_transfer" --drive-pacer-min-sleep="$fb_min_sleep"ms --drive-pacer-burst="$fb_BURST" --min-size "$fb_min_size"M --check-first
-echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  补缺完毕/n"
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  Completed/n"
 exit
