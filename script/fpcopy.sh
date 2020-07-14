650762ba9ea390b8853f84907202c9ce8b39abe3
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-14 06:48:18 +0530

    Update fpcopy.sh

--- script/fpcopy.sh
+++ script/fpcopy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #=============================================================
-# https://github.com/cgkings/fclone_shell_bot
+# https://github.com/roshanconnor123/fclone_shell_bot
 # File Name: fpcopy.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:点对点转存-copy
+# Description:Point-to-point-copy
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
-echo "等待ini配置变量更新"
+echo "Wait for ini configuration variable update"
 sleep 30s
 source ~/fclone_shell_bot/myfc_config.ini
 clear
-echo -e "▣▣▣▣▣▣执行转存▣▣▣▣▣▣"
+echo -e "▣▣▣▣▣▣Perform Copy▣▣▣▣▣▣"
 fclone copy "$fclone_name":{$fp_link1} "$fclone_name":{$fp_link2} --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first
-echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕/n"
-echo -e "▣▣▣▣▣▣执行补缺▣▣▣▣▣▣"
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  Copy Done/n"
+echo -e "▣▣▣▣▣▣Perform Copy▣▣▣▣▣▣"
 fclone copy "$fclone_name":{$fp_link1} "$fclone_name":{$fp_link2} --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers="$fp_chercker" --transfers="$fp_transfer" --drive-pacer-min-sleep="$fp_min_sleep"ms --drive-pacer-burst="$fp_BURST" --min-size "$fp_min_size"M --check-first
-echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  补缺完毕/n"
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  Copy Done/n"
 exit
