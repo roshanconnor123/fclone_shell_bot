b7a43b219a85d45cbf41c8cb428528d905dba037
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-14 06:50:49 +0530

    Update fptask.sh

--- script/fptask.sh
+++ script/fptask.sh
@@ -4,29 +4,29 @@
 # File Name: fptask.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:点对点转存-task
+# Description:Point-to-point-task
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
 source ~/fclone_shell_bot/myfc_config.ini
 clear
-read -p "请输入转存源ID==>" link1
+read -p "Please enter the source FolderID==>" link1
 if [ -z "$link1" ] ; then
-    echo "不允许输入为空" && exit
+    echo "No input is allowed" && exit
 else
 link1=${link1#*id=};link1=${link1#*folders/};link1=${link1#*d/};link1=${link1%?usp*}
 fi
-read -p "请输入转存目标ID==>" link2
+read -p "Please enter the destination FolderID==>" link2
 if [ -z "$link2" ] ; then
-    echo "不允许输入为空" && exit
+    echo "No input is allowed" && exit
 else
 link2=${link2#*id=};link2=${link2#*folders/};link2=${link2#*d/};link2=${link2%?usp*}
 sed -i "s/${fp_link1}/${link1}/g" /root/fclone_shell_bot/myfc_config.ini
 sed -i "s/${fp_link2}/${link2}/g" /root/fclone_shell_bot/myfc_config.ini
 fi
-echo -e "/n该模式暂不支持队列任务，仅支持后台任务"
+echo -e "/nThis mode does not support queue tasks, only background tasks"
 tmux new -s fptask -d
 tmux send -t "fptask" '~/gclone_shell_bot/script/fpcopy.sh' Enter
-echo -e "/n后台任务已开始执行"
+echo -e "/nBackground task has started"
 exit
