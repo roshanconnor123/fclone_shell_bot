0dcf95a43039888a1042a90124b8587f130c87fc
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-14 06:59:43 +0530

    Update fqtask.sh

--- script/fqtask.sh
+++ script/fqtask.sh
@@ -4,40 +4,40 @@
 # File Name: fqtask.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:极速版-task
+# Description:Extreme Edition-task
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
 source ~/fclone_shell_bot/myfc_config.ini
 clear
-read -p "请输入分享链接==>" link
+read -p "Please enter shared link==>" link
 link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
 rootname=$(fclone lsd "$fclone_name":{$link} --dump bodies -vv 2>&1 | awk 'BEGIN{FS="\""}/^{"id/{print $8}')
 if [ -z "$link" ] ; then
-echo "不允许输入为空" && exit ; 
+echo "No input is allowed" && exit ; 
 elif [ -z "$rootname" ] ; then
-echo "读取文件夹名称出错，请反馈问题给作者" && exit ; else
+echo "Error reading folder name, please give feedback to author" && exit ; else
 echo -e "$link" >> ~/fclone_shell_bot/log/fqtask.log
 fi
-read -t 5 -n1 -p "是否继续添加队列任务:[0.是/1.否]" task_stats
+read -t 5 -n1 -p "Whether to continue to add queue tasks: [0. Yes/1. No]" task_stats
 task_stats=${task_stats:-1}
 while [[ $task_stats -eq 0 ]];do
-    echo -e "/n继续添加队列任务"
-    read -p "请输入分享链接==>" link
+    echo -e "/nContinue to add queue tasks"
+    read -p "Please enter shared link==>" link
     link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
     rootname=$(fclone lsd "$fclone_name":{$link} --dump bodies -vv 2>&1 | awk 'BEGIN{FS="\""}/^{"id/{print $8}')
     if [ -z "$link" ] ; then
-    echo "不允许输入为空" && exit ; 
+    echo "No input is allowed" && exit ; 
     elif [ -z "$rootname" ] ; then
-    echo "读取文件夹名称出错，请反馈问题给作者" && exit ; else
+    echo "Error reading folder name, please give feedback to author" && exit ; else
     echo -e "$link" >> ~/fclone_shell_bot/log/fqtask.log
     fi
-    read -t 5 -n1 -p "是否继续添加队列任务:[0.是/1.否](默认1)" task_stats
+    read -t 5 -n1 -p "Whether to continue to add queue tasks: [0. yes/1. no] (default 1)" task_stats
     task_stats=${task_stats:-1}
 done
-echo -e "/n结束添加队列任务"
+echo -e "/nStarting queue task"
 tmux new -s fqtask -d
 tmux send -t "fqtask" '~/fclone_shell_bot/script/fqcopy.sh' Enter
-echo -e "/n队列任务已开始执行"
+echo -e "/nQueue task has started"
 exit
