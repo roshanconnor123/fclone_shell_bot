ad526f354922522b556727ba4f4a402df2109205
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-14 06:44:37 +0530

    Update fdedup.sh

--- script/fdedup.sh
+++ script/fdedup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #=============================================================
-# https://github.com/cgkings/fclone_shell_bot
+# https://github.com/roshanconnor123/fclone_shell_bot
 # File Name: fdedup.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:定向查重
+# Description:Remove Duplicates
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
 source ~/fclone_shell_bot/myfc_config.ini
 clear
-read -p "请输入要查重的链接==>" link
+read -p "Please enter the FolderID to check==>" link
 if [ -z "$link" ] ; then
-    echo "不允许输入为空" && exit
+    echo "No input is allowed" && exit
 else
 link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
 fi
-echo -e "▣▣▣▣▣▣正在执行查重▣▣▣▣▣▣"
-fclone dedupe newest "$fclone_name":{$link} --fast-list --size-only --drive-use-trash=false --no-traverse --checkers=64 --transfers=128 -q
-echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
+echo -e "▣▣▣▣▣▣Checking is being performed▣▣▣▣▣▣"
+fclone dedupe largest "$fclone_name":{$link} --fast-list --size-only --drive-use-trash=true --no-traverse --checkers=64 --transfers=128 -q
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  Removed Dupes"
