88effb18a99b0111f9f9820adb4783077e12a033
roshanconnor123 <56798243+roshanconnor123@users.noreply.github.com>
2020-07-14 07:03:29 +0530

    Update fsize.sh

--- script/fsize.sh
+++ script/fsize.sh
@@ -4,16 +4,16 @@
 # File Name: fsize.sh
 # Author: cgking
 # Created Time : 2020.7.8
-# Description:size查询
+# Description:size Calculation
 # System Required: Debian/Ubuntu
 # Version: final
 #=============================================================
 
 source ~/fclone_shell_bot/myfc_config.ini
 clear
-read -p "请输入查询链接==>" link
+read -p "Please enter the query link==>" link
 if [ -z "$link" ] ; then
-    echo "不允许输入为空" && exit
+    echo "No input is allowed" && exit
 else
 link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
 fi
@@ -22,6 +22,6 @@ size_info=`fclone size "$fclone_name":{$link} --checkers=200`
 file_num=$(echo "$size_info" | awk 'BEGIN{FS=" "}/^Total objects/{print $3}')
 file_size=$(echo "$size_info" | awk 'BEGIN{FS=" "}/^Total size/{print $3,$4}')
 echo -e "▣▣▣▣▣▣任务信息▣▣▣▣▣▣\n"
-echo -e "┋资源名称┋:"$rootname"\n"
-echo -e "┋资源数量┋:"$file_num"\n"
-echo -e "┋资源大小┋:"$file_size"\n"
+echo -e "┋Folder Name┋:"$rootname"\n"
+echo -e "┋Number of Files┋:"$file_num"\n"
+echo -e "┋Size of Files┋:"$file_size"\n"
